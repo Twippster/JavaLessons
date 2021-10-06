e4630993dc2a4d46e460d11d135ba867ee7a84b0
Twippster <64157577+Twippster@users.noreply.github.com>
2021-10-06 08:18:16 -0700

    Reupload

---
+++ com.javalearning/src/MultiplicationTableDisplayer.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MultiplicationTableDisplayer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int userInput = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(userInput + " * " + i + " = ");
+            System.out.printf("%2d\n", userInput * i);
+        }
+
+        // System.out.println(userInput + " * " + 1 + " = " + userInput * 1);
+        // System.out.println(userInput + " * " + 2 + " = " + userInput * 2);
+        // System.out.println(userInput + " * " + 3 + " = " + userInput * 3);
+        // System.out.println(userInput + " * " + 4 + " = " + userInput * 4);
+        // System.out.println(userInput + " * " + 5 + " = " + userInput * 5);
+        // System.out.println(userInput + " * " + 6 + " = " + userInput * 6);
+        // System.out.println(userInput + " * " + 7 + " = " + userInput * 7);
+        // System.out.println(userInput + " * " + 8 + " = " + userInput * 8);
+        // System.out.println(userInput + " * " + 9 + " = " + userInput * 9);
+        // System.out.println(userInput + " * " + 10 + " = " + userInput * 10);
+
+        sc.close();
+    }
+}
--- com.javalearning/src/MultiplicationTablesDisplayer.java
+++ com.javalearning/src/MultiplicationTablesDisplayer.java
@@ -1,14 +1,12 @@
-public class MultiiplicationTablesDisplayer {
+public class MultiplicationTablesDisplayer {
     public static void main(String[] args) {
         
         for (int i = 1; i <= 10; i++) {
             for (int n = 1; n <= 10; n++) {
                 System.out.print(n + " * " + i + " = ");
-                System.out.printf("%2d\t", n * i);            
-            }                                       
-            
+                System.out.printf("%2d\t", i * n);
+            }
             System.out.println();
-
         }
     }
 }
